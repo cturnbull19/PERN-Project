1b44c62f37844087899552c3b4a2febfef3259ae
Caroline Turnbull <cturnbull@carolines-mbp-2.myfiosgateway.com>
2024-01-16 14:32:09 -0500

    const required all my packages

--- server/index.js
+++ server/index.js
@@ -7,6 +7,24 @@ const PORT = 8080;
 app.use(cors());
 app.use(express.json());
 
+// init morgan
+const morgan = require('morgan');
+app.use(morgan('dev'));
+
+// init body-parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+// init db client
+const client = require('./db/client');
+client.connect();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// Router: /api
+app.use('/api', require('./api'));
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
