1a446dc0cd82b3eb7314c33131f1e6775a3226e6
Caroline Turnbull <cturnbull@carolines-mbp-2.myfiosgateway.com>
2024-01-16 12:38:41 -0500

    added more specific dummy data for my project

--- server/db/seedData.js
+++ server/db/seedData.js
@@ -1,6 +1,6 @@
 const client = require('./client');
 
-// drop tables for video games and board games
+// drop tables for clients, membership, exercises
 async function dropTables() {
     try {
         console.log('Dropping All Tables...');
@@ -18,23 +18,24 @@ async function createTables() {
     try {
         console.log('Building All Tables...');
         await client.query(`
-      CREATE TABLE videoGames (
+      CREATE TABLE clients (
         id SERIAL PRIMARY KEY,
-        name VARCHAR(255) UNIQUE NOT NULL,
-        description TEXT NOT NULL,
-        price INTEGER NOT NULL,
-        "inStock" BOOLEAN DEFAULT false,
-        "isPopular" BOOLEAN DEFAULT false,
-        "imgUrl" VARCHAR(255) DEFAULT 'https://i.imgur.com/3J3wW9S.png'
+        first_name VARCHAR(255) NOT NULL,
+        last_name VARCHAR(255) NOT NULL,
+        email VARCHAR(300) UNIQUE NOT NULL,
+        membership_id INT FOREIGN KEY REFERENCES membership(membership_id)
         );
-        CREATE TABLE boardGames (
-            id SERIAL PRIMARY KEY,
-            name VARCHAR(255) UNIQUE NOT NULL,
-            description TEXT NOT NULL,
+        CREATE TABLE membership (
+            membership_id SERIAL PRIMARY KEY,
+            type VARCHAR(50) NOT NULL,
+            expiration DATE,
             price INTEGER NOT NULL,
-            "inStock" BOOLEAN DEFAULT false,
-            "isPopular" BOOLEAN DEFAULT false,
-            "imgUrl" VARCHAR(255) DEFAULT 'https://i.imgur.com/3J3wW9S.png'
+            );
+            CREATE TABLE exercises (
+                id SERIAL PRIMARY KEY
+                name VARCHAR(255) UNIQUE NOT NULL,
+                description TEXT NOT NULL,
+                'imgURL' BYTEA DEFAULT './no-image.jpeg'
             );
         `);
     } catch (error) {
@@ -47,40 +48,28 @@ async function createInitialData() {
     try {
         console.log('Creating Initial Data...');
         await client.query(`
-      INSERT INTO videoGames (name, description, price, "inStock", "isPopular", "imgUrl")
+      INSERT INTO clients (first_name, last_name, email)
       VALUES
-        ('Final Fantasy VII', 'The best game ever!', 100, true, true, 'https://i.imgur.com/3J3wW9S.png'),
-        ('Final Fantasy VIII', 'The best game ever!', 100, true, true, 'https://i.imgur.com/3J3wW9S.png'),
-        ('Final Fantasy IX', 'The best game ever!', 100, true, true, 'https://i.imgur.com/3J3wW9S.png'),
-        ('Final Fantasy X', 'The best game ever!', 100, true, true, 'https://i.imgur.com/3J3wW9S.png'),
-        ('Final Fantasy X-2', 'The best game ever!', 100, true, true, 'https://i.imgur.com/3J3wW9S.png'),
-        ('Final Fantasy XII', 'The best game ever!', 100, true, true, 'https://i.imgur.com/3J3wW9S.png'),
-        ('Final Fantasy XIII', 'The best game ever!', 100, true, true, 'https://i.imgur.com/3J3wW9S.png'),
-        ('Final Fantasy XIV', 'The best game ever!', 100, true, true, 'https://i.imgur.com/3J3wW9S.png'),
-        ('Final Fantasy XV', 'The best game ever!', 100, true, true, 'https://i.imgur.com/3J3wW9S.png'),
-        ('Final Fantasy Tactics', 'The best game ever!', 100, true, true, 'https://i.imgur.com/3J3wW9S.png'),
-        ('Final Fantasy Tactics: The War of the Lions', 'The best game ever!', 100, true, true, 'https://i.imgur.com/3J3wW9S.png'),
-        ('Final Fantasy Tactics Advance', 'The best game ever!', 100, true, true, 'https://i.imgur.com/3J3wW9S.png'),
-        ('Final Fantasy Tactics A2: Grimoire of the Rift', 'The best game ever!', 100, true, true, 'https://i.imgur.com/3J3wW9S.png'),
-        ('Final Fantasy Crystal Chronicles', 'The best game ever!', 100, true, true, 'https://i.imgur.com/3J3wW9S.png')`
+        ('Caroline', 'Turnbull', 'carolinel@email.com'),
+        ('Annie', 'Turnbull', 'annie@email.com'),
+        ('Elizabeth', 'Tunbull', 'elizabeth@email.com'),
+        ('Doug', 'Turnbull', 'doug@email.com')
+        ('MJ', 'Turnbull', 'mj@email.com')`
         );
         await client.query(`
-        INSERT INTO boardGames (name, description, price, "inStock", "isPopular", "imgUrl")
+        INSERT INTO membership (type, expiration, price)
         VALUES
-            ('Catan', 'The best game ever!', 100, true, true, 'https://i.imgur.com/3J3wW9S.png'),
-            ('Ticket to Ride', 'The best game ever!', 100, true, true, 'https://i.imgur.com/3J3wW9S.png'),
-            ('Pandemic', 'The best game ever!', 100, true, true, 'https://i.imgur.com/3J3wW9S.png'),
-            ('Codenames', 'The best game ever!', 100, true, true, 'https://i.imgur.com/3J3wW9S.png'),
-            ('Scrabble', 'The best game ever!', 100, true, true, 'https://i.imgur.com/3J3wW9S.png'),
-            ('Monopoly', 'The best game ever!', 100, true, true, 'https://i.imgur.com/3J3wW9S.png'),
-            ('Clue', 'The best game ever!', 100, true, true, 'https://i.imgur.com/3J3wW9S.png'),
-            ('Risk', 'The best game ever!', 100, true, true, 'https://i.imgur.com/3J3wW9S.png'),
-            ('Battleship', 'The best game ever!', 100, true, true, 'https://i.imgur.com/3J3wW9S.png'),
-            ('Sorry!', 'The best game ever!', 100, true, true, 'https://i.imgur.com/3J3wW9S.png'),
-            ('Chess', 'The best game ever!', 100, true, true, 'https://i.imgur.com/3J3wW9S.png'),
-            ('Checkers', 'The best game ever!', 100, true, true, 'https://i.imgur.com/3J3wW9S.png'),
-            ('Backgammon', 'The best game ever!', 100, true, true, 'https://i.imgur.com/3J3wW9S.png'),
-            ('Go', 'The best game ever!', 100, true, true, 'https://i.imgur.com/3J3wW9S.png')`
+            ('ultimate', '2025-01-01', 250),
+            ('student', '2024-06-01', 100),
+            ('student', '2024-06-01', 100),
+            ('ultimate', '2025-01-01', 250)
+            ('teacher', '2025-01-01', 0)`
+        );
+        await client.query(`
+        INSERT INTO exercises (name, description, 'imgURL')
+        VALUES
+            ('squat', 'feet hip distance apart, engage your core, lower down as if sitting in an invisible chair, stop when knees are at about 90 degrees, straighten legs to stand back up.', './squat.png'),
+            ('plank', 'Lying on the ground with the elbows in line with the shoulder and the feet shoulder width apart, Push your body up bearing the weight on the forearms and feet, Keeping your body straight', './plank.jpeg')`
         );
     } catch (error) {
         throw error;
