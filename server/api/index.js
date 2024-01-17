a489b122c20b32244ea1e8bba9b31bfd989a4e5d
Caroline Turnbull <cturnbull@carolines-mbp-2.myfiosgateway.com>
2024-01-17 12:50:01 -0500

    fixed app crashing by inputting my files in api folder.

--- server/api/index.js
+++ server/api/index.js
@@ -1,4 +1,4 @@
-const express = requre('express');
+const express = require('express');
 const router = express.Router();
 
 //GET /api/health
--- server/db/sqlHelperFunctions/exercises.js
+++ server/db/sqlHelperFunctions/exercises.js
@@ -1,4 +1,4 @@
-const client = require('./client');
+const client = require('../client');
 const util = require('util');
 
 // GET - /api/exercises - get all exercises
