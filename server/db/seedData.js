6012ca96026cacf59cea72801da58a45954899cd
Caroline Turnbull <cturnbull@carolines-mbp-2.myfiosgateway.com>
2024-01-25 11:14:26 -0500

    added more exercises

--- server/db/seedData.js
+++ server/db/seedData.js
@@ -79,8 +79,12 @@ async function createInitialData() {
         await client.query(`
         INSERT INTO exercises (name, description, "imgURL")
         VALUES
-            ('squat', 'feet hip distance apart, engage your core, lower down as if sitting in an invisible chair, stop when knees are at about 90 degrees, straighten legs to stand back up.', 'squat.png'),
-            ('plank', 'Lying on the ground with the elbows in line with the shoulder and the feet shoulder width apart, Push your body up bearing the weight on the forearms and feet, Keeping your body straight', 'plank.jpeg')`
+            ('squat', 'feet hip distance apart, engage your core, lower down as if sitting in an invisible chair, stop when knees are at about 90 degrees, straighten legs to stand back up.', 'cat-squat.png'),
+            ('plank', 'Lying on the ground with the elbows in line with the shoulder and the feet shoulder width apart, Push your body up bearing the weight on the forearms and feet, Keeping your body straight', 'plank-cat.png'),
+            ('reverse lunge', 'To perform a reverse lunge, start by standing upright with your feet hip-width apart, then step backward with one foot, lowering your hips until both knees are bent at about a 90-degree angle. Make sure your front knee is directly above your ankle and your back knee is hovering just above the ground, then return to the starting position and repeat with the other leg.', 'lunge-cat.png'),
+            ('shoulder press', 'To perform a shoulder press, start by standing or sitting with your back straight, holding a weight in each hand at shoulder level with your elbows bent and palms facing forward. Push the weights upward until your arms are fully extended above your head, then slowly lower them back to the starting position.', 'shoulderpress-cat.png'),
+            ('bicep curl', 'To perform a bicep curl, stand with your feet shoulder-width apart, holding a weight in each hand with your arms fully extended and palms facing forward. Curl the weights towards your shoulders by bending your elbows, keeping your upper arms stationary, then slowly lower the weights back to the starting position.', 'bicepcurl-cat.png'),
+            ('jumping jacks', 'To perform jumping jacks, start by standing upright with your feet together and arms at your sides. Jump to spread your feet apart while simultaneously raising your arms overhead, then quickly jump back to the starting position.', 'jumpingjack-cat.png')`
         );
     } catch (error) {
         console.log('error creating initial data')
