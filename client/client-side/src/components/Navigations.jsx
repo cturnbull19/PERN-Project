0f4c042ca7278efc11e359daf89ebbbdebbe3502
Caroline Turnbull <cturnbull@carolines-mbp-2.myfiosgateway.com>
2024-01-25 16:28:10 -0500

    completely re-styled my home page and navbar. will continue this later

--- client/client-side/src/components/Navigations.jsx
+++ client/client-side/src/components/Navigations.jsx
@@ -14,22 +14,50 @@ const Navigations = () => {
     }
 
     return (
-        <div className='navBar'>
-            <Link to='/'>Home</Link>
-            <Link to='/exercises'>Exercise Library</Link>
+        <div className='navbar navbar-expand-md navbar-light'>
+            <div className='container-xxl'>
+                <div className='navbar-brand'>
+                    <Link to='/' className='fw-bold text-secondary'>Home</Link>
+                </div>
+                <div>
+                    <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#main-nav' aria-controls='main-nav' aria-expanded='false' aria-label='Toggle Navigation'>
+                        <span class='navbar-toggler-icon'></span>
+                    </button>
+                    <div className='collapse navbar-collapse justify-content-end align-center' id='main-nav'>
+                        <div className='navbar-nav'>
+                            <div className='nav-item'>
+                            <Link to='/exercises' className='nav-link'>Exercise Library</Link>
+                            </div>
+                            {(!token) ? (
+                                <>
+                                <div className='nav-item'>         
+                                <Link to='/register' className='nav-link'>Register</Link>
+                                </div> 
+                                <div className='nav-item'> 
+                                <Link to='/login' className='nav-link'>Login</Link>
+                                </div>
+                                </> 
+                    
+                            ) : (
+                                <>
+                                <div className='nav-item d-md-none'> 
+                                <button onClick={handleLogOut} className='nav-link'>Logout</button>
+                                </div>
 
-            {(!token) ? (
-                <>
-                <Link to='/register'>Register</Link>
-                <Link to='/login'>Login</Link>
-                </>
-            ) : (
-                <>
-                <button onClick={handleLogOut}>Logout</button>
-                <Link to='workoutBuilder'>WorkoutBuilder</Link>
-                </>
+                                <div className='nav-item ms-2 d-none d-md-inline'> 
+                                <button onClick={handleLogOut} className='btn btn-secondary'>Logout</button>
+                                </div>
+                        
+                                <div className='nav-item'> 
+                                <Link to='workoutBuilder' className='nav-link'>WorkoutBuilder</Link>
+                                </div>
+                                </>
 
-            )}
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
