a03c9d3ccf4285b6a5d9907744117b7a3acfaaf2
Caroline Turnbull <cturnbull@carolines-mbp-2.myfiosgateway.com>
2024-01-29 10:21:42 -0500

    deploy

--- server/db/client.js
+++ server/db/client.js
@@ -1,6 +1,6 @@
 const { Client } = require('pg');
 
-const connectionString = process.env.DATABASE_URL || 'https://localhost:54321/workoutlibrary';
+const connectionString = process.env.DATABASE_URL || 'postgres://workoutlibrary_user:dya6K73o0yLWCThJ43MiqqQmmQ88134E@dpg-cmrs37gl5elc73ak9kf0-a/workoutlibrary';
 
 const client = new Client({
   connectionString,
