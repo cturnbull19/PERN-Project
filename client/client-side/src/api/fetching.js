5aa79ffceb76d9b71f6c8f391f2d0853b2c2d5e5
Caroline Turnbull <cturnbull@carolines-mbp-2.myfiosgateway.com>
2024-01-29 10:38:25 -0500

    fixed api url

--- client/client-side/src/api/fetching.js
+++ client/client-side/src/api/fetching.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const fetchingApi = createApi({
     reducerPath: "fetchingApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:8080/api',
+        baseUrl: 'https://workoutbuilder.onrender.com/api',
     }),
 
     endpoints: (build) => ({
